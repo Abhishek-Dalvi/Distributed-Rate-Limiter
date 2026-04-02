1d5412c8845932aa1a8d2760b98e1a86f76dd3c6
Abhishek-Dalvi <abhishek1994dalvi@gmail.com>
2026-04-02 09:07:49 +0530

    Basic redis connection test

--- ratelimiter/ratelimiter/src/test/java/com/ratelimiter/RatelimiterApplicationTests.java
+++ ratelimiter/ratelimiter/src/test/java/com/ratelimiter/RatelimiterApplicationTests.java
@@ -1,13 +1,30 @@
 package com.ratelimiter;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import redis.clients.jedis.Jedis;
 
 @SpringBootTest
 class RatelimiterApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+	try (Jedis jedis = new Jedis("localhost", 6379)) {
+			// Store a key-value pair
+	        jedis.set("myKey", "Hello Redis!");
+	        
+	        // Retrieve the value
+	        String value = jedis.get("myKey");
+	        
+	        assertEquals("Hello Redis!", value);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
 	}
 
 }
